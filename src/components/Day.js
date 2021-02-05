a961237a3f9e65be06f03b6aa1e5d9b9aa28b48a
Mike Leonard <mtleonardjr@gmail.com>
2021-02-05 14:43:24 -0500

    added styling and components

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,9 +2,16 @@
 const Day = ({ day }) => {
 
     return (
-      <div className='day'>
-        <h1>{day.date}</h1>
-      </div>
+    <div className='day'>
+        <div className ='date'>{day.date}</div>
+        <div className='weather'>{day.weather}</div>
+
+        <div className='bottom'>
+        <span className='left'>{day.temp2m.min}</span>
+        <span className='right'>{day.temp2m.max}</span>
+        </div>
+    </div>
+    
     )
   }
   
