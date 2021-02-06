0d0cac41cd0ff338fe58d179314383cf2ffa9ea0
Mike Leonard <mtleonardjr@gmail.com>
2021-02-06 11:26:27 -0500

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Days from './components/Days'
+import Search from './components/Search'
 
 function App() {
 
+  //get weather route
+  const getWeather = async (zip) => {
+    const res = await fetch('http://localhost:5000/' + zip)
+    const data = await res.json()
+
+    return data
+  }
 
   return (
     <div className="container">
@@ -11,10 +19,8 @@ function App() {
       <div className='day-container'>
         <Days />  
       </div>
-      <div>
-        <p>
-          This will be search component
-        </p>
+      <div className='search-container'>
+        <Search />
       </div>
 
     </div>
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+
+const Search = () => {
+
+    return (
+      <form className='search'>
+        <label>Zip Code:</label>
+        <input type='Text' placeholder='21218'></input>
+        <input type='submit' className='btn'></input>
+      </form>
+    )
+  }
+  
+  export default Search
